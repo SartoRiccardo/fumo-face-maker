6f9a56eea21323df76db53131f7804d89845a4a6
SartoRiccardo <rikki.sarto@gmail.com>
2024-06-28 16:25:42 +0200

    feat: Better credits

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,15 +5,24 @@ export default function Footer() {
     <footer className={styles.footer}>
       <hr className={styles.hr} />
       <p>
-        Made by Sarto (@sartouhou)
+        Made by{" "}
+        <a href="https://www.instagram.com/sartouhou/" target="_blank">
+          @sartouhou
+        </a>{" "}
+        <i className="bi-instagram"></i> <i className="bi-twitter-x"></i>
         <br />
-        More information on how to make fumos on{" "}
+        More resources on how to make fumos on{" "}
         <a
           href="https://fumo.systems/posts/custom-fumo-making/"
           target="_blank"
         >
           fumo.systems
+        </a>{" "}
+        and the{" "}
+        <a href="https://discord.com/invite/fumofumo" target="_blank">
+          Fumo Discord
         </a>
+        .
       </p>
     </footer>
   );
