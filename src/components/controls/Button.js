761e054aba815924e4f2ae3622c108fbf39e74cc
SartoRiccardo <rikki.sarto@gmail.com>
2024-06-30 13:42:39 +0200

    feat: Can generate & download file

--- src/components/controls/Button.js
+++ src/components/controls/Button.js
@@ -1,8 +1,12 @@
 import "./Button.css";
 
-export default function Button({ children, onClick, className }) {
+export default function Button({ children, onClick, className, disabled }) {
   return (
-    <button className={`${className} shadow-sm`} onClick={onClick}>
+    <button
+      className={`${className} shadow-sm`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
--- src/components/controls/FileButtons.js
+++ src/components/controls/FileButtons.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import Button from "./Button";
+import { useAppSelector } from "@/lib/store";
+import { selectFumoFace } from "@/features/fumoFaceSlice";
+import download from "downloadjs";
 
 export default function FileButtons() {
+  const fumoFace = useAppSelector(selectFumoFace);
+  const [isDownloading, setDownloading] = useState(false);
+
+  const generateFace = async (_e) => {
+    setDownloading(true);
+    const params = new URLSearchParams({
+      eyes: fumoFace.eyes + 1,
+      eyelashes: fumoFace.eyelash + 1,
+      eyebrows: fumoFace.eyebrows + 1,
+      mouth: fumoFace.mouth + 1,
+      heterochromia: fumoFace.hasHeterochromia,
+    });
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND + "/face?" + params.toString()
+      );
+      const blob = await response.blob();
+      console.log(response.headers);
+      download(blob, "generated.DST", "application/octet-stream");
+    } catch (ex) {}
+    setDownloading(false);
+  };
+
   return (
     <div className="d-flex justify-content-center">
-      <Button className={"mx-2"}>
+      <Button
+        className={"mx-2"}
+        disabled={isDownloading}
+        onClick={generateFace}
+      >
         <i className="bi bi-download" />
       </Button>
       <Button className={"mx-2"}>
