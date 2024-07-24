045741371fb9638cf2596cfb5d78ca16423c88cd
SartoRiccardo <rikki.sarto@gmail.com>
2024-07-24 21:11:31 +0200

    feat: Color square input animations and ui

--- src/components/usercontrols/ThreadColor.js
+++ src/components/usercontrols/ThreadColor.js
@@ -1,8 +1,11 @@
+import { onSpacePress } from "@/utils/events";
 import styles from "./ThreadColor.module.css";
 
 export default function ThreadColor({ color, onClick }) {
   return onClick ? (
     <div
+      tabIndex={0}
+      onKeyDown={onSpacePress((_e) => onClick(_e))}
       onClick={onClick}
       className={`${styles.changable} ${styles.color}`}
       style={{ backgroundColor: color }}
